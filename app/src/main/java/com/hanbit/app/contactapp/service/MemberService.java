44dd1b460d54a061432da2620bbf09dcabc7dd03
pakjkwan <pakjkwan@gmail.com>
2017-02-11 12:11:05 +0900

    17-02-11 12:10

---
+++ app/src/main/java/com/hanbit/app/contactapp/service/MemberService.java
@@ -0,0 +1,23 @@
+package com.hanbit.app.contactapp.service;
+
+import com.hanbit.app.contactapp.domain.MemberBean;
+
+import java.util.ArrayList;
+
+/**
+ * Created by hb2000 on 2017-02-04.
+ */
+public interface MemberService {
+    //CREATE
+    public void add(MemberBean bean);
+    //READ ONE
+    public MemberBean findOne(MemberBean bean);
+    //READ SOME
+    public ArrayList<MemberBean> findSome(String keyword);
+    //READ ALL
+    public ArrayList<MemberBean> list();
+    //UPDATE
+    public void update(MemberBean bean);
+    //DELETE
+    public void delete(MemberBean bean);
+}
